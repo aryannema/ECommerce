58aecf5f0ecb198fd268832d99e07144ad87f26d
aryannema <aaryan.techy@gmail.com>
2026-03-26 23:16:12 +0530

    implemented header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/header";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+    </>
+  );
+};
+export default App;
