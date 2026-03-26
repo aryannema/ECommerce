7c18645bdee92031f20c30bdab95050177292813
aryannema <aaryan.techy@gmail.com>
2026-03-26 23:40:39 +0530

    implemented Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
           <h1>Welcome to Proshop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   );
 };
